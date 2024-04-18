0335852dfb9d96acc255ded95d9d709606eb066f
GUSTAVO PAZ <gustavo.paz@portalsesisp.org.br>
2024-04-18 18:17:52 -0300

    Images test

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -250,25 +250,25 @@ export default function Home() {
               <hr className="hrs hr3"></hr>
               <hr className="hrs hr4"></hr>
             </div>
+            {jsonProject
+              .filter((project) => project.name === 'NLWSpacetime')
+              .map((project) => (
+                <img
+                  key={project.id}
+                  className="img-project"
+                  data-aos="fade-right"
+                  src={project.url}
+                  alt={project.name}
+                  width={570}
+                  height={320}
+                  style={{ borderRadius: '30px' }}
+                />
+              ))}
             {displayedRepos.map((item) =>
               displayedRepos.indexOf(item) % 2 === 0 ? (
                 <div key={item.id} className="project-content">
                   <div className="box-content">
                     {/* <Link className="link-project" href={item.url}> */}
-                    {jsonProject
-                      .filter((project) => project.name === 'NLWSpacetime')
-                      .map((project) => (
-                        <img
-                          key={project.id}
-                          className="img-project"
-                          data-aos="fade-right"
-                          src={project.url}
-                          alt={project.name}
-                          width={570}
-                          height={320}
-                          style={{ borderRadius: '30px' }}
-                        />
-                      ))}
                     {/* </Link> */}
                   </div>
                   <div className="text-project" data-aos="fade-left">
